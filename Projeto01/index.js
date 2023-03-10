0b6dfc4e36113d9bc620805b2b774410a954b917
Douglas-g-s-Rocha <grocha0612@gmail.com>
2023-03-09 21:38:58 -0300

    funfando

--- Projeto01/index.js
+++ Projeto01/index.js
@@ -1,13 +1,32 @@
 import chalk from "chalk";
+import fs from "fs"; 
 
-console.log(chalk.red.bgYellow("Hello word!"))
-console.log(chalk.bgBlue("Hello word!"))
 
-const paragrafo = "Texto retornado por uma função";
+//console.log(chalk.red.bgYellow("Hello word!"))
+//console.log(chalk.bgBlue("Hello word!"))
 
-function texto(string){
-    return string
+//const paragrafo = "Texto retornado por uma função";
+
+//function texto(string){
+  //  return string
+//}
+
+//console.log(texto(paragrafo));
+
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, "ARQUIVO NÃO ENCONTRADO!"));
 }
 
-console.log(texto(paragrafo));
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+    //fs.readFile(caminhoDoArquivo, encoding, (_, texto) => { (o _ da como indefinido)
+        fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+            if(erro){
+                trataErro(erro);
+            }
+        console.log(chalk.green(texto));
+    })
+}
 
+pegaArquivo('./arquivos/texto.md'); //certo
+//pegaArquivo('./banana/texto.md'); // errado
